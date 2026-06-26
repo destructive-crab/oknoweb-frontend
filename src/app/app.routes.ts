c72b08f5700c735a9c8ec529d99aa4c4e0a169ac
destructiv_crab <destructivecrab@gmail.com>
2026-06-26 22:40:02 +0300

    render settings change

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Home } from './home/home';
 import { Submit } from './submit/submit';
 import { Submissions } from './submissions/submissions';
 import { SubmitSuccess } from './submit-success/submit-success';
+import { RenderMode } from '@angular/ssr';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -10,5 +11,8 @@ export const routes: Routes = [
   { path: 'home', component: Home },
   { path: 'submit', component: Submit },
   { path: 'submissions', component: Submissions },
-  { path: 'submit/success/:id', component: SubmitSuccess },
+  {
+    path: 'submit/success/:id',
+    component: SubmitSuccess,
+  },
 ];
