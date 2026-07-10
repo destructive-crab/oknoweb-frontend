a6947db3d5029ef5a8914fe0e291a32453b6be9e
will4ka <mishkor111@gmail.com>
2026-07-10 20:44:53 +0300

    add reject button to approved games on admin panel

--- dist/oknoweb/server/angular-app-manifest.mjs
+++ dist/oknoweb/server/angular-app-manifest.mjs
@@ -36,20 +36,20 @@ export default {
   {
     "renderMode": 2,
     "preload": [
-      "chunk-6U5TDE35.js"
+      "chunk-X6NB2MZB.js"
     ],
     "route": "/admin/submit"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 11525, hash: 'da58be1d8857186c0af96c6e57c5ff3139af1ca0c97c1a407fe0891f32d6de42', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 996, hash: '477ad23bdd0b2df06f60e25be43324b7fed8b7913c70d902422ea4b6f022b0ed', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'submit/index.html': {size: 18215, hash: '7717adddc13c35574fa85a1d0ededbc85a924b7595cdb8a9a64c95f135a208a3', text: () => import('./assets-chunks/submit_index_html.mjs').then(m => m.default)},
-    'home/index.html': {size: 26061, hash: 'e53857b37239de6efbba201752e381c1ce7e48f567b28553a0e74ee527427126', text: () => import('./assets-chunks/home_index_html.mjs').then(m => m.default)},
-    'admin/submit/index.html': {size: 16318, hash: 'ac5dda3276c8f3dccbbed412e25b8df5fa7db024fd21680db0e8aa02bc661627', text: () => import('./assets-chunks/admin_submit_index_html.mjs').then(m => m.default)},
-    'admin/login/index.html': {size: 17810, hash: '2ba912678f60a9f7010f176ff531cdd605c7793b606ce53f75d561b44b54466f', text: () => import('./assets-chunks/admin_login_index_html.mjs').then(m => m.default)},
-    'submissions/index.html': {size: 54460, hash: '596a86afc296542cf5574b89b6bc701ca3b917d71b1628fbc17572f6e6ba87d9', text: () => import('./assets-chunks/submissions_index_html.mjs').then(m => m.default)},
-    'styles-UBRZ63EA.css': {size: 49005, hash: '0tUkWhLcrSI', text: () => import('./assets-chunks/styles-UBRZ63EA_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 11525, hash: 'a5ab954e70425a05adb0d784e379b5c7a4e65061fe4c161382a47c0a89be30a7', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 996, hash: '2c21e50b3a48b302aacf2488ae78e74d815f496f68b82e2c24d73fa912e2126f', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'submit/index.html': {size: 18215, hash: '76bfb0329333ec11bf33cc6f9673ee6207d26bc3cb3a5e27452cf224428143bb', text: () => import('./assets-chunks/submit_index_html.mjs').then(m => m.default)},
+    'admin/login/index.html': {size: 17810, hash: 'a7efbf6103c5255739860780f6a77460cb4949d1b629b3427a1451f4f7f148ab', text: () => import('./assets-chunks/admin_login_index_html.mjs').then(m => m.default)},
+    'admin/submit/index.html': {size: 16300, hash: 'abd3a23b55b73f55321379933abc1f78cf629ac48c0cee1e606956e4ff23d287', text: () => import('./assets-chunks/admin_submit_index_html.mjs').then(m => m.default)},
+    'home/index.html': {size: 26061, hash: '401b382f20ac2cbfe57696c056c82c79bcdce45212ce25338bb4e013c2b24a95', text: () => import('./assets-chunks/home_index_html.mjs').then(m => m.default)},
+    'submissions/index.html': {size: 53648, hash: 'b149bdc5df34aa8f267dcbb95a89e2d525f6ed876959f4c7f73e8e6b5170b30f', text: () => import('./assets-chunks/submissions_index_html.mjs').then(m => m.default)},
+    'styles-IWEX4CAW.css': {size: 49078, hash: 'cyjHTj2Rh9c', text: () => import('./assets-chunks/styles-IWEX4CAW_css.mjs').then(m => m.default)}
   },
 };
