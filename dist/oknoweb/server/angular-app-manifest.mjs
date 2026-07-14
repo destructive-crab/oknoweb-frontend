ca3c3abebe30e15788e4e7cf7b45bac61b85065f
destructiv_crab <destructivecrab@gmail.com>
2026-07-14 14:24:33 +0300

    cache filter false test

--- dist/oknoweb/server/angular-app-manifest.mjs
+++ dist/oknoweb/server/angular-app-manifest.mjs
@@ -29,27 +29,27 @@ export default {
   {
     "renderMode": 2,
     "preload": [
-      "chunk-3C7OHGJ3.js"
+      "chunk-XEFDFNIG.js"
     ],
     "route": "/admin/login"
   },
   {
     "renderMode": 2,
     "preload": [
-      "chunk-7372G7K4.js"
+      "chunk-MQCLIZN2.js"
     ],
     "route": "/admin/submit"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 11549, hash: '32d5cfa4332f998a9d31f1e4ff5dd8649ede0943ca79785b418833d419cbe0f6', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 996, hash: 'b24f8b191c1b645340ac06f84b063b3914e35b5c46259fb5cda96fa11e99901a', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'home/index.html': {size: 29273, hash: 'a0317cf920442425cd49ac407e78ab93c73fc0515804bb55b433619a15191a53', text: () => import('./assets-chunks/home_index_html.mjs').then(m => m.default)},
-    'submit/index.html': {size: 18240, hash: '5ad43251e13b41dc4dcb18025d07c19c203e7025623ac964c9c02e7cbb9d6466', text: () => import('./assets-chunks/submit_index_html.mjs').then(m => m.default)},
-    'admin/login/index.html': {size: 17931, hash: '62cd64667f00d39a3deeb81285b0deecd4b1bb555071ad6a8aa1d31f1f7a87ab', text: () => import('./assets-chunks/admin_login_index_html.mjs').then(m => m.default)},
-    'admin/submit/index.html': {size: 18233, hash: '796eb35abcb540d624d33c18fbbd3ac062b39fd74fd4154f63afe5798b54abef', text: () => import('./assets-chunks/admin_submit_index_html.mjs').then(m => m.default)},
-    'submissions/index.html': {size: 58961, hash: '11f3282c6f02047051d20f5203f1f1f255d92a6ae3652922d9f5c8d524359862', text: () => import('./assets-chunks/submissions_index_html.mjs').then(m => m.default)},
-    'styles-4SUHADTH.css': {size: 50399, hash: 'Tf1SMmKx7pA', text: () => import('./assets-chunks/styles-4SUHADTH_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 10771, hash: '2f0d85273cbe6faefa4ee8214cee05908ddaa7aa455e84669a53798081d60a28', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 996, hash: '3d92f24ea4b1019eaf47c8ea9d6d8f3391f535840152193b1e01e6bdd4695c78', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'submit/index.html': {size: 17431, hash: '478a560a08c5fdb3d8230aaf2185fa3478bbf4d954cb420a67996a61e439048f', text: () => import('./assets-chunks/submit_index_html.mjs').then(m => m.default)},
+    'home/index.html': {size: 28267, hash: '40c310f993c9838c897d233252986c533874287bead983c6d82135cdba5fa0f7', text: () => import('./assets-chunks/home_index_html.mjs').then(m => m.default)},
+    'admin/login/index.html': {size: 17077, hash: 'affab2f82c40ed1868ac28d89fc5caf2bea7b8db5dc3949020bc13ee83dba56d', text: () => import('./assets-chunks/admin_login_index_html.mjs').then(m => m.default)},
+    'admin/submit/index.html': {size: 17377, hash: 'b3102b73d55e43c227ec4ee33ca493c19afdce61d32743386350f347f213b963', text: () => import('./assets-chunks/admin_submit_index_html.mjs').then(m => m.default)},
+    'submissions/index.html': {size: 21701, hash: 'da85a002a437d5dad36a844aefdaff9ad9bab5b7727aa9dbca251a9dd455f14d', text: () => import('./assets-chunks/submissions_index_html.mjs').then(m => m.default)},
+    'styles-O2L3GKWC.css': {size: 47654, hash: 'BkWQRH69IBQ', text: () => import('./assets-chunks/styles-O2L3GKWC_css.mjs').then(m => m.default)}
   },
 };
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,19 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import { provideClientHydration, withHttpTransferCacheOptions } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideHttpClient(),
-    provideRouter(routes), provideClientHydration()
+    provideRouter(routes), provideClientHydration(),
+    provideHttpClient(withFetch()),
+    provideClientHydration(
+      withHttpTransferCacheOptions({
+        filter: (req) => false,
+      })
+    )
   ]
 };
