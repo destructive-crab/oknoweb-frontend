1d2b8ad5adbd3925c2489b4febb0083e28cc41f5
destructiv_crab <destructivecrab@gmail.com>
2026-07-14 14:27:46 +0300

    cache filter true test

--- dist/oknoweb/server/angular-app-manifest.mjs
+++ dist/oknoweb/server/angular-app-manifest.mjs
@@ -43,13 +43,13 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 10771, hash: '2f0d85273cbe6faefa4ee8214cee05908ddaa7aa455e84669a53798081d60a28', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 996, hash: '3d92f24ea4b1019eaf47c8ea9d6d8f3391f535840152193b1e01e6bdd4695c78', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'submit/index.html': {size: 17431, hash: '478a560a08c5fdb3d8230aaf2185fa3478bbf4d954cb420a67996a61e439048f', text: () => import('./assets-chunks/submit_index_html.mjs').then(m => m.default)},
-    'home/index.html': {size: 28267, hash: '40c310f993c9838c897d233252986c533874287bead983c6d82135cdba5fa0f7', text: () => import('./assets-chunks/home_index_html.mjs').then(m => m.default)},
-    'admin/login/index.html': {size: 17077, hash: 'affab2f82c40ed1868ac28d89fc5caf2bea7b8db5dc3949020bc13ee83dba56d', text: () => import('./assets-chunks/admin_login_index_html.mjs').then(m => m.default)},
-    'admin/submit/index.html': {size: 17377, hash: 'b3102b73d55e43c227ec4ee33ca493c19afdce61d32743386350f347f213b963', text: () => import('./assets-chunks/admin_submit_index_html.mjs').then(m => m.default)},
-    'submissions/index.html': {size: 21701, hash: 'da85a002a437d5dad36a844aefdaff9ad9bab5b7727aa9dbca251a9dd455f14d', text: () => import('./assets-chunks/submissions_index_html.mjs').then(m => m.default)},
-    'styles-O2L3GKWC.css': {size: 47654, hash: 'BkWQRH69IBQ', text: () => import('./assets-chunks/styles-O2L3GKWC_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 11227, hash: 'a0b690883218704f1a4ddf7b78d3047e8387bddd3991eb3e704159633eb61ba4', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 996, hash: 'cd3f975a12d12837c5e33c5abdccf11e41f8216797114fef23ea46b0cd6b923d', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'admin/login/index.html': {size: 17600, hash: 'd5cf8ac6d558dc2884157f7c5dbcf214a36387b797a12ef02810c155f2e03396', text: () => import('./assets-chunks/admin_login_index_html.mjs').then(m => m.default)},
+    'submit/index.html': {size: 17909, hash: '93e53b85efb611211a8e30cde8e7a538566a9a9c82bda6cfa408ef12be89b1bd', text: () => import('./assets-chunks/submit_index_html.mjs').then(m => m.default)},
+    'home/index.html': {size: 28942, hash: '3d26bd7f7c86ca3652939d79fa1e0b4d61fb61ca5a92c4e86046f8d7523b3080', text: () => import('./assets-chunks/home_index_html.mjs').then(m => m.default)},
+    'admin/submit/index.html': {size: 17901, hash: '933ee7d4c49f16bcfc8f6ed25823a204140ec51aba0148ab096ac8b7d35fb193', text: () => import('./assets-chunks/admin_submit_index_html.mjs').then(m => m.default)},
+    'submissions/index.html': {size: 58837, hash: 'af42b957eee144a684fe902888f69f0dcc02df8edbe0532936845cb47a4831e1', text: () => import('./assets-chunks/submissions_index_html.mjs').then(m => m.default)},
+    'styles-UHIMA4NL.css': {size: 49281, hash: 'NgLd2xaIXdQ', text: () => import('./assets-chunks/styles-UHIMA4NL_css.mjs').then(m => m.default)}
   },
 };
