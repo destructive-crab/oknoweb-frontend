9aaae6cf46b4c5a06ef1d6e04f48a75a27149de3
destructiv_crab <destructivecrab@gmail.com>
2026-08-16 18:42:07 +0300

    build

--- dist/oknoweb/server/angular-app-manifest.mjs
+++ dist/oknoweb/server/angular-app-manifest.mjs
@@ -45,8 +45,8 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 11366, hash: 'c34efaa36b11d59c2f8becd1e96038397773499de0df5dc3196c08306a8f6454', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 945, hash: '77dad40459e18979f1733a52e8dbc2e0421b0936015067c00b7f0ff6272ec067', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 11366, hash: 'db504e1ee3f959db7f6e02d5625103d046459c88611db23d6bc5d725bae14c5a', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 945, hash: 'b4ef68b2b1d9a97a415b87be51dac99507004d3aec9d80ce1a68490aeabb2673', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-B3DH6AEL.css': {size: 51339, hash: '452q33KTq/Y', text: () => import('./assets-chunks/styles-B3DH6AEL_css.mjs').then(m => m.default)}
   },
 };
